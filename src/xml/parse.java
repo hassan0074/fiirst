748711605099fe01ab551a33009660a6085cc30e
Executioner_rip <hjaved.bscs16seecs@seecs.edu.pk>
2017-12-29 18:49:59 -0800

    Add prompt user

--- src/xml/parse.java
+++ src/xml/parse.java
@@ -17,5 +17,8 @@ public class parse {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    public void promptuser()
+    {
+        
+    }
 }
